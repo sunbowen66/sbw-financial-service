72b2e9a874508a504a838f4393fc15ce1d09be52
sunbowen <1194784024@qq.com>
2024-04-27 21:55:25 +0800

    轮询检测是否登录

--- sbw-common/src/main/java/com/sbw/common/config/ALiSmsConfig.java
+++ sbw-common/src/main/java/com/sbw/common/config/ALiSmsConfig.java
@@ -1,37 +1,37 @@
-//package com.sbw.common.config;
-//
-//import com.aliyun.auth.credentials.Credential;
-//import com.aliyun.auth.credentials.provider.StaticCredentialProvider;
-//import com.aliyun.sdk.service.dysmsapi20170525.AsyncClient;
-//import darabonba.core.client.ClientOverrideConfiguration;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//@ConditionalOnProperty(prefix = "ali.sms", name = "enable", havingValue = "true")
-//public class ALiSmsConfig {
-//    final ALiProperties aLiProperties;
-//
-//    @Bean
-//    public AsyncClient asyncClient() {
-//        StaticCredentialProvider provider = StaticCredentialProvider.create(Credential.builder()
-//                .accessKeyId(aLiProperties.getAccessKeyId())
-//                .accessKeySecret(aLiProperties.getAccessKeySecret())
-//                .build());
-//
-//        // Configure the Client
-//        AsyncClient client = AsyncClient.builder()
-//                .region(aLiProperties.getSms().getRegion()) // Region ID
-//                .credentialsProvider(provider)
-//                .overrideConfiguration(
-//                        ClientOverrideConfiguration.create()
-//                                .setEndpointOverride(aLiProperties.getSms().getEndpoint())
-//                )
-//                .build();
-//        return client;
-//    }
-//}
+package com.sbw.common.config;
+
+import com.aliyun.auth.credentials.Credential;
+import com.aliyun.auth.credentials.provider.StaticCredentialProvider;
+import com.aliyun.sdk.service.dysmsapi20170525.AsyncClient;
+import darabonba.core.client.ClientOverrideConfiguration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "ali.sms", name = "enable", havingValue = "true")
+public class ALiSmsConfig {
+    final ALiProperties aLiProperties;
+
+    @Bean
+    public AsyncClient asyncClient() {
+        StaticCredentialProvider provider = StaticCredentialProvider.create(Credential.builder()
+                .accessKeyId(aLiProperties.getAccessKeyId())
+                .accessKeySecret(aLiProperties.getAccessKeySecret())
+                .build());
+
+        // Configure the Client
+        AsyncClient client = AsyncClient.builder()
+                .region(aLiProperties.getSms().getRegion()) // Region ID
+                .credentialsProvider(provider)
+                .overrideConfiguration(
+                        ClientOverrideConfiguration.create()
+                                .setEndpointOverride(aLiProperties.getSms().getEndpoint())
+                )
+                .build();
+        return client;
+    }
+}
--- sbw-common/src/main/java/com/sbw/common/config/ElasticsearchConfig.java
+++ sbw-common/src/main/java/com/sbw/common/config/ElasticsearchConfig.java
@@ -1,59 +1,59 @@
-//package com.sbw.common.config;
-//
-//import co.elastic.clients.elasticsearch.ElasticsearchClient;
-//import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-//import co.elastic.clients.transport.ElasticsearchTransport;
-//import co.elastic.clients.transport.rest_client.RestClientTransport;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.http.Header;
-//import org.apache.http.HttpHost;
-//import org.apache.http.message.BasicHeader;
-//import org.elasticsearch.client.RestClient;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@ConfigurationProperties(prefix = "elasticsearch")
-//@ConditionalOnProperty("elasticsearch.host")
-//@Slf4j
-//@RequiredArgsConstructor
-//public class ElasticsearchConfig {
-//
-//    @Value("${elasticsearch.host}")
-//    private String host;
-//
-//    @Value("${elasticsearch.port}")
-//    private int port;
-//
-//    @Value("${elasticsearch.scheme:http}")
-//    private String scheme;
-//
-//    @Value("${elasticsearch.apiKey:}")
-//    private String apiKey;
-//
-//    final ObjectMapper objectMapper;
-//
-//    @Bean
-//    public ElasticsearchClient elasticsearchClient() {
-//        log.info(">>>>>>>>>>> ElasticsearchClient config init.");
-//        RestClient restClient = RestClient.builder(
-//                new HttpHost(host, port, scheme))
-//                .setDefaultHeaders(
-//                        new Header[]{
-//                                new BasicHeader("Authorization", "ApiKey " + apiKey)
-//                        }
-//                )
-//                .build();
-//        ElasticsearchTransport transport = new RestClientTransport(
-//                restClient, new JacksonJsonpMapper(objectMapper));
-//        ElasticsearchClient client = new ElasticsearchClient(transport);
-//
-//        log.info("ElasticsearchClient init end");
-//        return client;
-//    }
-//}
+package com.sbw.common.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "elasticsearch")
+@ConditionalOnProperty("elasticsearch.host")
+@Slf4j
+@RequiredArgsConstructor
+public class ElasticsearchConfig {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.port}")
+    private int port;
+
+    @Value("${elasticsearch.scheme:http}")
+    private String scheme;
+
+    @Value("${elasticsearch.apiKey:}")
+    private String apiKey;
+
+    final ObjectMapper objectMapper;
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        log.info(">>>>>>>>>>> ElasticsearchClient config init.");
+        RestClient restClient = RestClient.builder(
+                new HttpHost(host, port, scheme))
+                .setDefaultHeaders(
+                        new Header[]{
+                                new BasicHeader("Authorization", "ApiKey " + apiKey)
+                        }
+                )
+                .build();
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper(objectMapper));
+        ElasticsearchClient client = new ElasticsearchClient(transport);
+
+        log.info("ElasticsearchClient init end");
+        return client;
+    }
+}
